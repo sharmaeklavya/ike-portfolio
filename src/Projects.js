990ba0cf912e512ce9e5019742c065c0c7358609
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2022-03-23 22:38:24 +0530

    updated proj texts

--- src/Projects.js
+++ src/Projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     name: "Invoice App",
     description:
-      "The App has three level of user access - Admin, Manager and Executive. These users have different level of privileges, where Admin can create, update, view and delete; Managers can create, update and view, followed by Executives who can only create and view invoices.",
+      "It is an Accounting Invoice Application. The App has three levels of user access - Admin, Manager and Executive. An admin can register an nth number of users. These users are provided with different levels of privileges; where an admin can create, update, view and delete invoices. Managers can create, update and view, followed by executives who can only create and view invoices.",
     demoEmail: "eklavya@email.com",
     demoPass: 123456,
     liveDemo: "https://proj-invoice.netlify.app",
@@ -19,7 +19,7 @@ const projects = [
   {
     name: "Delivery App",
     description:
-      "This is a Pizza Delivery App. The app allows users to create an account with the app. Users can add pizzas, toppings and thier favourite beverages to the cart. Users can make payments and check the order status. The App also tracks all the previous orders made by a customer.",
+      "It is a Pizza Delivery app. The app allows users to create an account with the app. Users can add Pizzas, Toppings and their favourite Beverages to the cart. Users can make payments and check the order status. The app also tracks all the previous orders made by a customer.",
     demoEmail: "eklavya@email.com",
     demoPass: "eklavya@12",
     liveDemo: "https://proj-delivery.herokuapp.com",
@@ -37,7 +37,7 @@ const projects = [
   {
     name: "Community App",
     description:
-      "This app allows users to share their thoughts, ideas and belief. Users can write their own article or share their opinion on any previously listed article. Users may create, comment but can delete only their articles. Users can also see author name and the date it was posted. Users will be required to create an account with the app in order to Create a Post, Comment or if he/she/they want(s) to delete their posts.",
+      "This app allows users to share their thoughts, ideas and belief. Users can write articles or share their opinion on any previously listed article. While users may create, comment but can only delete their written articles. Users can also see the author's name and the date it was posted. Users will be required to create an account with the app in order to Create a Post, Comment or if he/she/they want(s) to delete their posts.",
     demoEmail: "eklavya@email.com",
     demoPass: "eklavya@12",
     liveDemo: "https://proj-community.herokuapp.com",
@@ -54,7 +54,7 @@ const projects = [
   {
     name: "Chatter App",
     description:
-      "This is a Chat Application. The app has a list of room/topics. A user can enter with his/her name on the login page, select a room and start chatting with people having similar interests. Send a text or emojis to express themselves. On the left side, app displays room name and names of all the persons who are currently in the room. The app also displays the information on the user joining a room or leaving a room.",
+      "It is a Chat Application. The app has a list of rooms/topics. A user can enter with his/her/their name on the login page, select a room and start chatting with people having similar interests. A user can send a text or emojis to express themselves. On the left side, the app displays room names and names of all the persons who are currently in the room. The app also displays the information on the user joining a room or leaving a room.",
     demoEmail: "Not required",
     demoPass: "Not required",
     liveDemo: "https://proj-chatter.netlify.app",
